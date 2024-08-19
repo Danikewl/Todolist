c73980a694bc21adb36ef6ff031a2ca0a44d4db4
Daniela <dani.kewlz@gmail.com>
2024-08-19 18:01:45 -0400

    Primer Commit: Estructura y archivos

---
+++ js/app.js
@@ -0,0 +1,18 @@
+//variables
+const formulario = document.querySelector("#formulario");
+const listaTweets = document.querySelector("#lista-tweets");
+let tweets = [];
+
+// eventListener
+eventListeners()
+
+function eventListeners() {
+    formulario.addEventListener("submit", agregarTweet)
+}
+
+//Funciones
+
+function agregarTweet(e) {
+    e.preventDefault()
+    console.log("Agregando Tweet")
+}
